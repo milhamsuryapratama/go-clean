ebef10df984d85a584a4ff76ed90375ae218cbd8
Ilham Surya Pratama <ilhamsurya26@gmail.com>
2020-09-29 09:58:13 +0700

    initial route group and run server

--- main.go
+++ main.go
@@ -1,5 +1,22 @@
 package main
 
+import (
+	"go-clean/config"
+	"go-clean/controller"
+	"go-clean/handler"
+	"go-clean/repository"
+
+	"github.com/gin-gonic/gin"
+)
+
 func main() {
-	return
+	r := gin.Default()
+	db := config.Connect()
+
+	repo := repository.NewKelasRepository(db)
+	entity := controller.NewKelasEntity(repo)
+	api := r.Group("/api")
+	handler.KelasHandlerFunc(api, entity)
+
+	r.Run()
 }
