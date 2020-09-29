1c8a388766527bce02b424d64316ff24bfc53d9d
Ilham Surya Pratama <ilhamsurya26@gmail.com>
2020-09-29 09:54:42 +0700

    create domain kelass with entity and repository interface

---
+++ domain/kelas.go
@@ -0,0 +1,23 @@
+package domain
+
+import (
+	"context"
+)
+
+// Kelas is ...
+type Kelas struct {
+	ID        int
+	NamaKelas string `json:"nama_kelas"`
+}
+
+// KelasEntity ...
+type KelasEntity interface {
+	Get(ctx context.Context) ([]Kelas, error)
+	Create(kelas Kelas) (Kelas, error)
+}
+
+// KelasRepository ...
+type KelasRepository interface {
+	Get(ctx context.Context) (res []Kelas, err error)
+	Create(kelas Kelas) (Kelas, error)
+}
