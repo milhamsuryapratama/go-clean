85261800d0b2de2cf02a0dc1d11dba5e3c8a9ab0
Ilham Surya Pratama <ilhamsurya26@gmail.com>
2020-10-02 20:07:33 +0700

    remove preload in get kelas function

--- repository/kelasRepository.go
+++ repository/kelasRepository.go
@@ -20,7 +20,7 @@ func NewKelasRepository(Conn *gorm.DB) domain.KelasRepository {
 // Get ...
 func (m *KelasRepository) Get(ctx context.Context) (res []domain.Kelas, err error) {
 	var kelas []domain.Kelas
-	m.Conn.Preload("Siswa").Find(&kelas)
+	m.Conn.Find(&kelas)
 	return kelas, nil
 }
 
