f635f113cfd919a0401cfb24f1763ac6934df31a
Ilham Surya Pratama <ilhamsurya26@gmail.com>
2020-10-02 20:07:13 +0700

    create database migrations

--- config/koneksi.go
+++ config/koneksi.go
@@ -1,6 +1,8 @@
 package config
 
 import (
+	"go-clean/domain"
+
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
@@ -9,5 +11,14 @@ import (
 func Connect() *gorm.DB {
 	dsn := "user:pass@tcp(127.0.0.1:3306)/dbname?charset=utf8mb4&parseTime=True&loc=Local"
 	db, _ := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+
+	hasKelasTable := db.Migrator().HasTable(domain.Kelas{})
+	// hasSiswaTable := db.Migrator().HasTable(domain.Siswa{})
+	if !hasKelasTable {
+		db.AutoMigrate(domain.Kelas{})
+		db.AutoMigrate(domain.Siswa{})
+	}
+	// db.Migrator().DropTable(domain.Kelas{})
+	// db.Migrator().DropTable(domain.Siswa{})
 	return db
 }
